51695089d27cbbbbcd93040f3cc19241f93f5a7c
daniela <danielamarie118@outlook.com>
2026-03-14 11:51:17 -0400

    Add IUserRepository

---
+++ src/Smdb.Core/Users/IUserRepository.cs
@@ -0,0 +1,12 @@
+namespace Smdb.Core.Users;
+
+using Shared.Http;
+
+public interface IUserRepository
+{
+    public Task<PagedResult<User>?> ReadUsers(int page, int size);
+    public Task<User?> CreateUser(User newUser);
+    public Task<User?> ReadMUser(int id);
+    public Task<User?> UpdateUser(int id, User newData);
+    public Task<User?> DeleteUser(int id);
+}
