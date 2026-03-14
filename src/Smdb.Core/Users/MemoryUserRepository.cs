24622db2f01d66be92a3f5b45a6f4f8b6a790bdd
daniela <danielamarie118@outlook.com>
2026-03-14 11:58:50 -0400

    Add MemoryUserRepository

---
+++ src/Smdb.Core/Users/MemoryUserRepository.cs
@@ -0,0 +1,61 @@
+namespace Smdb.Core.Users;
+
+using Shared.Http;
+using Smdb.Core.Db;
+
+public class MemoryUserRepository : IUserRepository
+{
+    private MemoryDatabase db;
+
+    public MemoryUserRepository(MemoryDatabase db)
+    {
+        this.db = db;
+    }
+
+    public async Task<PagedResult<User>?> ReadUsers(int page, int size)
+    {
+        int totalCount = db.Users.Count;
+        int start = Math.Clamp((page - 1) * size, 0, totalCount);
+        int length = Math.Clamp(size, 0, totalCount - start);
+        var values = db.Users.Slice(start, length);
+        var result = new PagedResult<User>(totalCount, values);
+
+        return await Task.FromResult(result);
+    }
+
+    public async Task<User?> CreateUser(User newUser)
+    {
+        newUser.Id = db.NextUserId();
+        db.Users.Add(newUser);
+
+        return await Task.FromResult(newUser);
+    }
+
+    public async Task<User?> ReadUser(int id)
+    {
+        User? result = db.Users.FirstOrDefault(m => m.Id == id);
+        return await Task.FromResult(result);
+    }
+    public async Task<User?> UpdateUser(int id, User newData)
+    {
+        User? result = db.Users.FirstOrDefault(m => m.Id == id);
+
+        if (result != null)
+        {
+            result.UserName = newData.UserName;
+            result.Year_Birth = newData.Year_Birth;
+            result.Email = newData.Email;
+        }
+
+        return await Task.FromResult(result);
+    }
+
+    public async Task<User?> DeleteUser(int id)
+    {
+        User? result = db.Users.FirstOrDefault(m => m.Id == id);
+
+        if (result != null) { db.Users.Remove(result); }
+
+        return await Task.FromResult(result);
+    }
+}
